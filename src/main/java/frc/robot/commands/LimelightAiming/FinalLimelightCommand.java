0ef73d51b766a9a6c9046cdfce5cb37c994119fd
Team 811 <team811cardinals@gmail.com>
2022-01-20 20:04:11 -0500

    test for little file commits for idiots - crane

--- src/main/java/frc/robot/commands/LimelightAiming/FinalLimelightCommand.java
+++ src/main/java/frc/robot/commands/LimelightAiming/FinalLimelightCommand.java
@@ -1,13 +1,11 @@
 package frc.robot.commands.LimelightAiming;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Vision.LimelightFetch;
 import frc.robot.subsystems.Drivetrain;
 
 public class FinalLimelightCommand extends CommandBase{
     private Drivetrain requiredSubsystem;
-    private double left_command;
-    private double right_command;
+
   
     public FinalLimelightCommand(Drivetrain m_SubsystemBase) {
       requiredSubsystem = m_SubsystemBase;
---
+++ src/main/java/frc/robot/subsystems/idiots.java
@@ -0,0 +1,5 @@
+package frc.robot.subsystems;
+
+public class idiots {
+    //little file thing
+}
